3da0f078b43b6efc61421d8a0125aefaa855d8a3
Lillie Mackey <lamackey@cps.edu>
2023-09-11 09:53:45 -0500

    rock paper scissors code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,15 @@
 # We will write a rock paper scissors game in class - Complete it in this file
+
+player_choice = "rock"
+computer_choice = "paper"
+
+# Create afunction that gets the choices
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice}
+
+    return choices
+
+choices = get_choices()
+print(choices)
